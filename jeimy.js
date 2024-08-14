c7d24c4837b281ff35677e087e7fe934f1e3d934
JEIMYJJ <jeimy_jjimenez@soy.sena.edu.co>
2024-08-13 20:50:39 -0500

    Tabla de multiplicar del 1 al 10

---
+++ jeimy.js
@@ -0,0 +1,8 @@
+// Define los números de la tabla y el límite aquí
+const numMul = 5;  
+const numLimit = 10;  
+
+console.log(`Tabla de multiplicar del ${numMul} hasta el multiplicador ${numLimit}`);
+for (let i = 1; i <= numLimit; i++) {
+    console.log(`${numMul} x ${i} = ${numMul * i}`);
+}
