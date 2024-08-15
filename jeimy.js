da532ed027968784c901a38ef4e092ee04a2aa66
JEIMYJJ <jeimy_jjimenez@soy.sena.edu.co>
2024-08-14 19:41:52 -0500

    Tabla de multiplicar

---
+++ jeimy.js
@@ -0,0 +1,8 @@
+// Define los números de la tabla y el límite aquí
+const numMul =7;  // Define el número de la tabla de multiplicar
+const numLimit = 10;  // Define el límite de multiplicación
+
+console.log(`Tabla de multiplicar del ${numMul} hasta el multiplicador ${numLimit}`);
+for (let i = 1; i <= numLimit; i++) {
+    console.log(`${numMul} x ${i} = ${numMul * i}`);
+}
